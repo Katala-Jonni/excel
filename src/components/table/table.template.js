07801b7e8ec0fe9f08f8c29579931ed3a196d48f
Максим <maksim@iMac-Maksim.local>
2020-11-20 00:53:39 +0300

    Finish base framewor functionalk

---
+++ src/components/table/table.template.js
@@ -0,0 +1,51 @@
+const CODES = {
+    A: 65,
+    Z: 90
+};
+
+function createRow(content, index = '') {
+    return `
+        <div class="row">
+        <div class="row-info">${index}</div>
+        <div class="row-data">${content}</div>
+        </div>`;
+}
+
+function toCell(content) {
+    return `
+        <div class="cell" contenteditable>${content}</div>
+    `;
+}
+
+function toColumn(content) {
+    return `
+        <div class="column">${content}</div>
+    `;
+}
+
+function toChar(_, index) {
+    return String.fromCharCode(CODES.A + index);
+}
+
+export function createTable(rowsCounts = 20) {
+    const colsCount = CODES.Z - CODES.A + 1;
+    const rows = [];
+    const cols = new Array(colsCount)
+        .fill('')
+        .map(toChar)
+        .map(toColumn)
+        .join(' ');
+
+    const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join(' ');
+
+
+    rows.push(createRow(cols, ''));
+    for (let i = 0; i < rowsCounts; i++) {
+        rows.push(createRow(cells, i + 1));
+    }
+
+    return rows.join('');
+}
